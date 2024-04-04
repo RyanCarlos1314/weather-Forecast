08ee9c33eb9d90842c67d27085b34e81534a20f1
Ryan Carlos <108019925+RyanCarlos1314@users.noreply.github.com>
2024-04-04 17:54:51 -0300

    Adicionando API no codigo

---
+++ src/javascript/script.js
@@ -0,0 +1,15 @@
+document.querySelector('#search').addEventListener('submit', (Event) => {
+    Event.preventDefault();
+
+    const cityName = document.querySelector('#city_name').value;
+
+    if (!cityName) {
+        return showAlert('Você precisa digitar uma cidade...');
+    }
+
+    console.log(cityName)
+});
+
+function showAlert(msg){
+    document.querySelector('#alert').innerHTML = msg;
+}
